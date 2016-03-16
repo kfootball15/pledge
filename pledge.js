3ec1d30b365a1a8afee75f35bd1f9d396f202e91
Jeff <fenster.js@gmail.com>
2016-03-16 18:30:39 -0400

    latest and greatest

--- pledge.js
+++ pledge.js
@@ -4,6 +4,82 @@ Promises Workshop: build the pledge.js deferral-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
+var $Promise = function(){
+	this.handlerGroups = [];
+	this.state = 'pending';
+};
+
+var Deferral = function(){
+	this.$promise = new $Promise();
+};
+
+var defer = function(){
+	return new Deferral();
+}
+
+Deferral.prototype.resolve = function(data){
+	if (this.$promise.state === 'pending') {
+		this.$promise.state = 'resolved'; 
+		this.$promise.value = data;		
+	}
+}
+
+Deferral.prototype.reject = function(reason){
+	if (this.$promise.state === 'pending'){
+		this.$promise.state = 'rejected';
+		this.$promise.value = reason;
+	}
+}
+
+$Promise.prototype.then = function(successfulFunc, errorFunc) {
+	var obj = {};
+
+	if (typeof successfulFunc == 'function') {
+		obj.successCb = successfulFunc;
+		// this.callHandlers(this.value);
+	} else {
+		obj.successCb = false;
+	}
+	if (typeof errorFunc == 'function') {
+		obj.errorCb = errorFunc;
+		// this.callHandlers(this.value);
+	} else {
+		obj.errorCb = false;
+	}
+
+	this.handlerGroups.push(obj);
+	
+
+	this.callHandlers(this.handlerGroups[0]);
+	// this.callHandlers(this.handlerGroups[0]);
+};
+
+$Promise.prototype.callHandlers = function(arrayObj) {
+	if (typeof arrayObj.successCb == 'function' && typeof arrayObj.errorCb == 'function'){
+		if (this.state === 'resolved') {
+			arrayObj.successCb();
+		} else {
+			arrayObj.errorCb();
+		}		
+	} else if (typeof arrayObj.successCb === 'function') {
+		if (this.state === 'resolved') {
+			arrayObj.successCb();
+		} else {
+			return false;
+		}		
+	} else if (typeof arrayObj.errorCb === 'function') {
+		if (this.state === 'resolved') {
+			return false;
+		} else {
+			arrayObj.errorCb();
+		}		
+	}
+	// else {
+	// 	return false;
+	// }
+
+}
+
 
 
 
